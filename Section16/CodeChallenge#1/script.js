bd4b0cdb0b533e188163c2ac0d2aa2609504ef80
PabloSato <pablofersato@gmail.com>
2022-08-18 09:31:51 +0200

    fin CodeChallenge#1 SECTION15

---
+++ Section16/CodeChallenge#1/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const renderCountry = function (data, className = '') {
+  const html = `
+            <article class="country ${className}">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)}M people</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+              </div>
+            </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(res => {
+      console.log(res);
+      if (!res.ok) throw new Error(`Problem with geocoding - ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`Your are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Country not found - ${response.status}`);
+
+      return response.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message}!!!`));
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.837, 72.873);
+whereAmI(-33.933, 18.474);
